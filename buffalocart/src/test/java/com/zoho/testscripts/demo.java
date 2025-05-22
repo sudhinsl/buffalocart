61d2d3fb27336f22b6aca0dd8be84ceec231d550
Dell <Dell@DESKTOP-V34R8TE>
2025-05-22 12:27:06 +0530

    modiy

--- buffalocart/src/test/java/com/zoho/testscripts/demo.java
+++ buffalocart/src/test/java/com/zoho/testscripts/demo.java
@@ -2,6 +2,7 @@ package com.zoho.testscripts;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.helpers.Reporter;
 import org.testng.annotations.Test;
 
 public class demo {
@@ -11,6 +12,7 @@ public class demo {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
+		driver.getTitle();
 		driver.quit();
 	}
 }
